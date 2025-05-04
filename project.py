0bd6b078cdfbd165944382036311a96c99babab2
mtchms <163765759+mtchms@users.noreply.github.com>
2025-05-04 18:05:31 +0300

    Update project.py

--- project.py
+++ project.py
@@ -10,7 +10,6 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
 
-
 CSV_PATH = 'styles.csv'
 IMAGES_DIR = 'images'
 IMG_SIZE = (128, 128)
